3d93ae388699368623bae8e53c8965468fb0bce7
nagammanikanta <nagammanikanta@gmail.com>
2024-03-18 10:52:47 +0530

    init

---
+++ gcsbucket.tf
@@ -0,0 +1,24 @@
+resource "google_storage_bucket" "gcs-bucket" {
+  name          = "pavan-gcs-bucket"
+
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "project_id" {
+  type   = string
+  description = "The GCP project ID"
+  default = "abstract-stage-417502"
+}
+
+variable "region" {
+  type   = string
+  description = "The GCP region"
+  default = "us-central1"
+}
+
+variable "zone" {
+  type   = string
+  description = "The GCP zone"
+  default = "us-central1-a"
+}
+
+variable "gcs-bucket" {
+  type   = string
+  description = "The name of the GCS bucket"
+  default = "abstract-stage-417502-bucket"
+}
+
