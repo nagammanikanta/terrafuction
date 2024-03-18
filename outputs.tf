d6ce548c2fa0f038e7f0855d41b6dcc0ced25166
nagammanikanta <nagammanikanta@gmail.com>
2024-03-18 15:48:00 +0530

    init

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,3 @@
-output "gcs-bucket" {
-  value = google_storage_bucket.gcs-bucket
-
-  description = "The name of the GCS bucket to create"
+output "bucket" {
+  value = google_storage_bucket.gcs-bucket.name
 }
