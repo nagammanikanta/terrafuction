eff2edfae2650438dfcafb28a363356c00f70777
nagammanikanta <nagammanikanta@gmail.com>
2024-03-22 15:30:02 +0530

    init

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,65 @@
-/*
+
 
 output "bucket" {
   value = google_storage_bucket.gcs-bucket.name
 }
-*/
+output "instance" {
+  value     = google_compute_instance.instance_terra
+  sensitive = true
+
+}
+
+output "custom" {
+  value     = google_compute_network.custom-vpc-tf
+  sensitive = true
+}
+
+output "subnet" {
+  value     = google_compute_subnetwork.subnet-tf
+  sensitive = true
+}
+output "ip" {
+  value     = google_compute_subnetwork.subnet-tf.ip_cidr_range
+  sensitive = true
+}
+output "firewall" {
+  value     = google_compute_firewall.firewall-terra
+  sensitive = true
+}
+
+
+
+output "sa_tf" {
+  value     = google_service_account.sa_tf
+  sensitive = true
+}
+output "vpc-1" {
+  value     = google_compute_network.vpc-1
+  sensitive = true
+}
+output "vpc-2" {
+  value     = google_compute_network.vpc-2
+  sensitive = true
+}
+output "vpc-3" {
+  value     = google_compute_network.vpc-3
+  sensitive = true
+
+}
+output "peering1" {
+  value     = google_compute_network_peering.peering1
+  sensitive = true
+}
+output "peering2" {
+  value     = google_compute_network_peering.peering2
+  sensitive = true
+}
+output "peering3" {
+  value     = google_compute_network_peering.peering3
+  sensitive = true
+}
+output "peering4" {
+  value     = google_compute_network_peering.peering4
+  sensitive = true
+
+}
--- variables.tf
+++ variables.tf
@@ -65,7 +65,7 @@ variable "source_tags" {
 
 variable "sa_tf_account_id" {
 
-  default = "tfmanikanta"
+  default = "pavan"
 
 }
 variable "sa_tf_display_name" {
