3b6a29664aaecd906b180e9f565679a15fbd7688
nagammanikanta <nagammanikanta@gmail.com>
2024-04-18 16:34:39 +0530

    init

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "5.20.0"
+      source = "hashicorp/google"
+      version = "5.25.0"
     }
   }
 }
